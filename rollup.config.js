0ac6e0c73c28fdc0081b14a1feba506b6e9b3803
Tim Haywood <tim@haywood.org>
2020-08-17 19:22:50 +1000

    build new version before publishing

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import pkg from "./package.json";
 
 export default {
   input: "src/index.js",
+  external: Object.keys(pkg.dependencies),
   output: [
     { file: pkg.main, format: "cjs", exports: "auto" },
     { file: pkg.module, format: "es" },
