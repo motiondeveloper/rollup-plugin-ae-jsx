5353e971fc43156431efe2843987f56aa96b8ba3
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2024-09-07 12:29:37 -0700

    wrap bundled code in `get()`

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { walk } from "estree-walker";
 import MagicString from "magic-string";
 
 const whitespace = /\s/;
+
 // these will be removed
 const disallowedNodeTypes = [
   "ExpressionStatement",
@@ -10,13 +11,15 @@ const disallowedNodeTypes = [
   "ExportNamedDeclaration",
 ];
 
-export default function afterEffectsJsx(options = {}) {
+// these will also be removed
+const expressionGlobals = ["thisComp", "thisLayer", "thisProperty"];
+
+export default function afterEffectsJsx(options = { wrap: false }) {
   const exports = [];
   return {
     name: "after-effects-jsx", // this name will show up in warnings and errors
     generateBundle(options = {}, bundle, isWrite) {
-      // format each file
-      // to be ae-jsx
+      // format each file to be ae-jsx
       for (const file in bundle) {
         // Get the string code of the file
         let code = bundle[file].code;
@@ -75,68 +78,102 @@ export default function afterEffectsJsx(options = {}) {
           },
         });
 
-        // Remove non exported nodes and convert
-        // to object property style compatible syntax
-        walk(ast, {
-          enter(node, parent) {
-            Object.defineProperty(node, "parent", {
-              value: parent,
-              enumerable: false,
-              configurable: true,
-            });
+        if (options.wrap) {
+          walk(ast, {
+            enter(node, parent) {
+              Object.defineProperty(node, "parent", {
+                value: parent,
+                enumerable: false,
+                configurable: true,
+              });
 
-            if (node.type === "FunctionDeclaration") {
-              // Deal with functions
-              const functionName = node.id.name;
-              if (!exports.includes(functionName)) {
-                // Remove non-exported functions
-                remove(node.start, node.end);
-              } else {
-                // remove the function keyword
-                magicString.remove(node.start, node.id.start);
-                // add a trailing comma
-                magicString.appendLeft(node.end, ",");
+              if (node.type === "VariableDeclaration") {
+                const variableName = node.declarations.map(
+                  (declaration) => declaration.id.name
+                )[0];
+                if (!expressionGlobals.includes(variableName)) {
+                  // Remove variables that aren't exported
+                  remove(node.start, node.end);
+                }
+                // don't process child nodes
+                this.skip();
+              } else if (disallowedNodeTypes.includes(node.type)) {
+                // Remove every top level node that isn't
+                // a function or variable, as they're not allowed
+                removeStatement(node);
+                this.skip();
               }
-              // don't process child nodes
-              this.skip();
-            } else if (node.type === "VariableDeclaration") {
-              // deal with variables
-              const variableName = node.declarations.map(
-                (declaration) => declaration.id.name
-              )[0];
-              if (!exports.includes(variableName)) {
-                // Remove variables that aren't exported
-                remove(node.start, node.end);
-              } else {
-                const valueStart = node.declarations[0].init.start;
-                const variableName = node.declarations[0].id.name;
-                // remove anything before the variable name
-                // e.g. const, var, let
-                magicString.overwrite(
-                  node.start,
-                  valueStart - 1,
-                  `${variableName}:`
-                );
-                const endsInSemiColon =
-                  magicString.slice(node.end - 1, node.end) === ";";
-                if (endsInSemiColon) {
-                  // replace ; with ,
-                  magicString.overwrite(node.end - 1, node.end, ",");
+            },
+          });
+
+          magicString
+            .prepend("get() {\n")
+            .append(`\nreturn {${exports.join(",\n\t")}}\n}`);
+        } else {
+          // Remove non exported nodes and convert
+          // to object property style compatible syntax
+          walk(ast, {
+            enter(node, parent) {
+              Object.defineProperty(node, "parent", {
+                value: parent,
+                enumerable: false,
+                configurable: true,
+              });
+
+              if (node.type === "FunctionDeclaration") {
+                // Deal with functions
+                const functionName = node.id.name;
+                if (!exports.includes(functionName)) {
+                  // Remove non-exported functions
+                  remove(node.start, node.end);
                 } else {
-                  // or add trailing comma
+                  // remove the function keyword
+                  magicString.remove(node.start, node.id.start);
+                  // add a trailing comma
                   magicString.appendLeft(node.end, ",");
                 }
+                // don't process child nodes
+                this.skip();
+              } else if (node.type === "VariableDeclaration") {
+                // deal with variables
+                const variableName = node.declarations.map(
+                  (declaration) => declaration.id.name
+                )[0];
+                if (!exports.includes(variableName)) {
+                  // Remove variables that aren't exported
+                  remove(node.start, node.end);
+                } else {
+                  const valueStart = node.declarations[0].init.start;
+                  const variableName = node.declarations[0].id.name;
+                  // remove anything before the variable name
+                  // e.g. const, var, let
+                  magicString.overwrite(
+                    node.start,
+                    valueStart - 1,
+                    `${variableName}:`
+                  );
+                  const endsInSemiColon =
+                    magicString.slice(node.end - 1, node.end) === ";";
+                  if (endsInSemiColon) {
+                    // replace ; with ,
+                    magicString.overwrite(node.end - 1, node.end, ",");
+                  } else {
+                    // or add trailing comma
+                    magicString.appendLeft(node.end, ",");
+                  }
+                }
+                // don't process child nodes
+                this.skip();
+              } else if (disallowedNodeTypes.includes(node.type)) {
+                // Remove every top level node that isn't
+                // a function or variable, as they're not allowed
+                removeStatement(node);
+                this.skip();
               }
-              // don't process child nodes
-              this.skip();
-            } else if (disallowedNodeTypes.includes(node.type)) {
-              // Remove every top level node that isn't
-              // a function or variable, as they're not allowed
-              removeStatement(node);
-              this.skip();
-            }
-          },
-        });
+            },
+          });
+        }
+
         // Log exports to the terminal
         console.log(`Exported JSX:`, exports);
         // Sanitize output and wrap in braces
