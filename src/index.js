c0011ad32a8056f4a88729828e2f59cb3fe61254
Tim Haywood <tim@haywood.org>
2024-10-18 13:09:23 +1100

    remove ts based jsx formatting

--- src/index.js
+++ src/index.js
@@ -111,10 +111,9 @@ export default function afterEffectsJsx(options = { wrap: false }) {
             .prepend('function get() {\n')
             .append(`\nreturn {${exports.join(',\n\t')}}\n}`);
 
-          const asString = format(magicString.toString()).replace(
-            `function get() {`,
-            `get() {`
-          );
+          const asString = magicString
+            .toString()
+            .replace(`function get() {`, `get() {`);
 
           magicString = new MagicString(asString);
         } else {
@@ -191,72 +190,4 @@ export default function afterEffectsJsx(options = { wrap: false }) {
       }
     },
   };
-}
-
-// extra: use ts to format... though currently not a dep of `rollup-plugin-ae-jsx`
-
-// https://github.com/vvakame/typescript-formatter/tree/master/lib
-import * as ts from 'typescript';
-
-const defaultOptions = {
-  baseIndentSize: 0,
-  indentSize: 2,
-  tabSize: 2,
-  indentStyle: ts.IndentStyle.Smart,
-  newLineCharacter: '\r\n',
-  convertTabsToSpaces: true,
-  insertSpaceAfterCommaDelimiter: true,
-  insertSpaceAfterSemicolonInForStatements: true,
-  insertSpaceBeforeAndAfterBinaryOperators: true,
-  insertSpaceAfterConstructor: false,
-  insertSpaceAfterKeywordsInControlFlowStatements: true,
-  insertSpaceAfterFunctionKeywordForAnonymousFunctions: false,
-  insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis: false,
-  insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets: false,
-  insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces: true,
-  insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces: false,
-  insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces: false,
-  insertSpaceAfterTypeAssertion: false,
-  insertSpaceBeforeFunctionParenthesis: false,
-  placeOpenBraceOnNewLineForFunctions: false,
-  placeOpenBraceOnNewLineForControlBlocks: false,
-  insertSpaceBeforeTypeAnnotation: false,
-};
-
-class LanguageServiceHost {
-  constructor() {
-    this.files = {};
-    // for ts.LanguageServiceHost
-    this.getCompilationSettings = () => ts.getDefaultCompilerOptions();
-    this.getScriptFileNames = () => Object.keys(this.files);
-    this.getScriptVersion = (_fileName) => '0';
-    this.getScriptSnapshot = (fileName) => this.files[fileName];
-    this.getCurrentDirectory = () => process.cwd();
-    this.getDefaultLibFileName = (options) => ts.getDefaultLibFilePath(options);
-  }
-  addFile(fileName, text) {
-    this.files[fileName] = ts.ScriptSnapshot.fromString(text);
-  }
-}
-
-function format(text, fileName = 'temp.ts', options = defaultOptions) {
-  const host = new LanguageServiceHost();
-  host.addFile(fileName, text);
-
-  const languageService = ts.createLanguageService(host);
-  const edits = languageService.getFormattingEditsForDocument(
-    fileName,
-    options
-  );
-
-  edits
-    .sort((a, b) => a.span.start - b.span.start)
-    .reverse()
-    .forEach((edit) => {
-      const head = text.slice(0, edit.span.start);
-      const tail = text.slice(edit.span.start + edit.span.length);
-      text = `${head}${edit.newText}${tail}`;
-    });
-
-  return text;
 }
